d9b18e1976d643784801fe50d826cf116c9cb1da
rochelleshen <rochellecshen@gmail.com>
2023-04-22 23:06:13 -0700

    mixed up qwerty

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -115,9 +115,9 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp M,             &kp C,             &kp 
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, T),    &kp G,             &kp H,             U_MT(LSHFT, N),    U_MT(LCTRL, M),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-U_LT(U_BUTTON, SLASH),       U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp J,             &kp K,             &kp COMMA,         U_MT(RALT, DOT),   \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp F,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp SQT,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, T),    &kp C,             &kp H,             U_MT(LSHFT, N),    U_MT(LCTRL, M),    U_MT(LALT, L),     U_MT(LGUI, P),   \
+U_LT(U_BUTTON, SLASH),       U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp G,             &kp V,             &kp B,             &kp J,             &kp K,             &kp COMMA,         U_MT(RALT, DOT),   \
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTZ \
